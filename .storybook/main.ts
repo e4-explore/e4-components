840642edc95323712257062153738025f63b5b0f
ethanphilipgrove-dot <ethanphilipgrove@gmail.com>
2026-07-18 17:05:04 -0400

    storybook verbiage update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-native-web-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
+  staticDirs: ['./public'],
   addons: [],
   framework: {
     name: '@storybook/react-native-web-vite',
---
+++ .storybook/manager.ts
@@ -0,0 +1,35 @@
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming';
+
+const e4Theme = create({
+  base: 'light',
+  brandTitle: 'E4 Explore',
+  brandUrl: 'https://github.com/ethanphilipgrove-dot',
+  brandImage: 'e4-logo.svg',
+  brandTarget: '_blank',
+
+  colorPrimary: '#2A2A33',
+  colorSecondary: '#4B7BFF',
+
+  appBg: '#FAFAF7',
+  appContentBg: '#FAFAF7',
+  appPreviewBg: '#FAFAF7',
+  appBorderColor: '#E2E2DA',
+  appBorderRadius: 10,
+
+  textColor: '#2A2A33',
+  textMutedColor: '#6E6E78',
+  barBg: '#FAFAF7',
+  barTextColor: '#6E6E78',
+  barSelectedColor: '#2A2A33',
+  barHoverColor: '#4B7BFF',
+
+  inputBg: '#FFFFFF',
+  inputBorder: '#2A2A33',
+  inputTextColor: '#2A2A33',
+  inputBorderRadius: 8,
+});
+
+addons.setConfig({
+  theme: e4Theme,
+});
